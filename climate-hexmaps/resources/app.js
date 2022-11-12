e3e293fd8d9881d31172a709c7ac5533d2f0c774
Hannah Woodward <hannah@woodward.sh>
2022-11-12 19:37:04 +0000

    Add colourbar, multiple colourscales, header, and improve styling

--- climate-hexmaps/resources/app.js
+++ climate-hexmaps/resources/app.js
@@ -1,17 +1,34 @@
 (() => {
   const data_config = {
-    'ann_rain_2019': { 'colourscale': 'viridis' },
-    'ann_rain_change_mmday_2065': { 'colourscale': 'viridis' },
-    'ann_warming_oC_2065': { 'colourscale': 'viridis' },
-    'drought_day_change_2065': { 'colourscale': 'viridis' },
-    'max5day_rain_mm_change_2065': { 'colourscale': 'viridis' },
-    'number_heatwave_days_change_2065': { 'colourscale': 'viridis' },
-    'warmest_dayofyear_change_oC_2065': { 'colourscale': 'viridis' }
+    'ann_rain_2019': { 'colourscale': 'Viridis' },
+    'ann_rain_change_mmday_2065': { 'colourscale': 'Viridis' },
+    'ann_warming_oC_2065': { 'colourscale': 'RdYlBu_r' },
+    'drought_day_change_2065': { 'colourscale': 'Viridis' },
+    'max5day_rain_mm_change_2065': { 'colourscale': 'Viridis' },
+    'number_heatwave_days_change_2065': { 'colourscale': 'RdYlBu_r' },
+    'warmest_dayofyear_change_oC_2065': { 'colourscale': 'RdYlBu_r' }
   };
 
   const colourscales = {
-    'viridis': new colourScale([{'rgb':[68,1,84],v:0},{'rgb':[72,35,116],'v':0.1},{'rgb':[64,67,135],'v':0.2},{'rgb':[52,94,141],'v':0.3},{'rgb':[41,120,142],'v':0.4},{'rgb':[32,143,140],'v':0.5},{'rgb':[34,167,132],'v':0.6},{'rgb':[66,190,113],'v':0.7},{'rgb':[121,209,81],'v':0.8},{'rgb':[186,222,39],'v':0.9},{'rgb':[253,231,36],'v':1}])
-  };
+    'Viridis': (min, max) => chroma.scale('Viridis').domain([min, max]),
+    'RdYlBu_r': (min, max) => chroma.scale('RdYlBu').domain([max, min])
+  }
+
+  // Colourscales from https://open-innovations.org/projects/hexmaps/builder.html
+  // const colourscales = {
+  //   'Viridis': 'rgb(122,76,139) 0%, rgb(124,109,168) 12.5%, rgb(115,138,177) 25%, rgb(107,164,178) 37.5%, rgb(104,188,170) 50%, rgb(133,211,146) 62.5%, rgb(189,229,97) 75%, rgb(254,240,65) 87.5%, rgb(254,240,65) 100%',
+  //   'ODI': 'rgb(114,46,165) 0%, rgb(230,0,124) 50%, rgb(249,188,38) 100%',
+  //   'Heat': 'rgb(0,0,0) 0%, rgb(128,0,0) 25%, rgb(255,128,0) 50%, rgb(255,255,128) 75%, rgb(255,255,255) 100%',
+  //   'IMD-low-high': 'rgb(8,64,129) 0%, rgb(8,104,172) 10%, rgb(43,140,190) 20%, rgb(78,179,211) 30%, rgb(123,204,196) 40%, rgb(168,221,181) 50%, rgb(204,235,197) 60%, rgb(224,243,219) 70%, rgb(238,252,217) 80%, rgb(251,252,244) 90%, rgb(251,252,244) 100%',
+  //   'IMD-high-low': 'rgb(251,252,244) 0%, rgb(238,252,217) 10%, rgb(224,243,219) 20%, rgb(204,235,197) 30%, rgb(168,221,181) 40%, rgb(123,204,196) 50%, rgb(78,179,211) 60%, rgb(43,140,190) 70%, rgb(8,104,172) 80%, rgb(8,64,129) 90%, rgb(8,64,129) 100%',
+  //   'Planck': 'rgb(0,0,255) 0%, rgb(0,112,255) 16.666%, rgb(0,221,255) 33.3333%, rgb(255,237,217) 50%, rgb(255,180,0) 66.666%, rgb(255,75,0) 100%',
+  //   'EPC': '#ef1c3a 1%, #ef1c3a 20.5%, #f78221 20.5%, #f78221 38.5%, #f9ac64 38.5%, #f9ac64 54.5%, #ffcc00 54.5%, #ffcc00 68.5%, #8cc63f 68.5%, #8cc63f 80.5%, #1bb35b 80.5%, #1bb35b 91.5%, #00855a 91.5%, #00855a 100%',
+  //   'Plasma': 'rgb(12,7,134) 0%, rgb(82,1,163) 12.5%, rgb(137,8,165) 25%, rgb(184,50,137) 37.5%, rgb(218,90,104) 50%, rgb(243,135,72) 62.5%, rgb(253,187,43) 75%, rgb(239,248,33) 87.5%',
+  //   'Referendum': '#4BACC6 0%, #B6DDE8 50%, #FFF380 50%, #FFFF00 100%',
+  //   'Leodis': '#2254F4 0%, #F9BC26 50%, #ffffff 100%',
+  //   'Longside': '#801638 0%, #addde6 100%',
+  //   'Black': '#000000 0%, #000000 100%'
+  // };
 
   const hex = new ODI.hexmap(
     document.querySelector('.hexmap__outer'),
@@ -27,10 +44,16 @@
           hexmap_select.appendChild(hexmap_opt)
         }
 
+        // Store some useful attrs for later
+        hex.extra = {
+          'activeKey': '',
+          'colourbar': document.querySelector('.hexmap__colourbar')
+        }
+
         // Update hexmap & add auto-update to select change
-        updateHexmap(hex, data_config, hexmap_select.value)
+        updateHexmap(hex, data_config, colourscales, hexmap_select.value)
         hexmap_select.addEventListener('change', e => {
-          updateHexmap(hex, data_config, e.target.value)
+          updateHexmap(hex, data_config, colourscales, e.target.value)
         });
       }
     }
@@ -48,7 +71,7 @@
     }
 
     region_name = e.data.data.n;
-    region_value = e.data.data[e.data.hexmap.activeKey] || 'No data';
+    region_value = e.data.data[e.data.hexmap.extra.activeKey] || 'No data';
     tip.innerHTML = region_name + '<br>' + region_value;
 
     const bb = hex.getBoundingClientRect();
@@ -57,51 +80,45 @@
     tip.style.top = Math.round(bb.top + bb.height/2 - bbo.top) + 'px';
   });
 
-  function colourScale(c) {
-    let s = c;
-    let n = s.length;
-    // Get a colour given a value, and the range minimum/maximum
-    this.getValue = (v, min, max) => {
-      if (Number.isNaN(v) || typeof v === 'undefined') {
-        return 'rgb(225, 225, 225)';
-      }
+  hex.el.addEventListener('mouseleave', () => {
+    const tooltip = hex.el.querySelector('.tooltip');
+    tooltip && tooltip.remove();
+  });
 
-      v = (v - min) / (max - min);
-      if (v < 0) return 'rgb('+s[0].rgb.join(',')+')';
-      if (v >= 1) return 'rgb('+s[n-1].rgb.join(',')+')';
-      for (let c = 0; c < n - 1; c++) {
-        const a = s[c];
-        const b = s[c+1];
-        if (v >= a.v && v < b.v) {
-          pc = Math.min(1, (v - a.v) / (b.v - a.v));
-          rgb = [
-            Math.round(a.rgb[0] + (b.rgb[0] - a.rgb[0]) * pc),
-            Math.round(a.rgb[1] + (b.rgb[1] - a.rgb[1]) * pc),
-            Math.round(a.rgb[2] + (b.rgb[2] - a.rgb[2]) * pc)
-          ];
-          return 'rgb(' + rgb.join(',') + ')';
-        }
-      }
-    };
-
-    return this;
-  }
-
-  function updateHexmap(obj, all_data_config, key) {
+  function updateHexmap(obj, all_data_config, colourscales, key) {
     if (!(key in all_data_config)) {
       console.error('Error: ' + key + ' not found in config');
       return;
     }
 
-    const config = all_data_config[key];
-    const colourscale = colourscales[config.colourscale];
     const data = Object.values(obj.mapping.hexes)
       .map(item => item[key] || Number.NaN)
       .filter(item => !Number.isNaN(item));
+    const vmin = Math.min(...data);
+    const vmax = Math.max(...data);
 
-    vmin = Math.min(...data);
-    vmax = Math.max(...data);
-    obj.updateColours(r => colourscale.getValue(obj.mapping.hexes[r][key], vmin, vmax));
-    obj.activeKey = key
+    const config = all_data_config[key];
+    // NB `colourscale_full`, `colourscale_norm` are chroma.scale objects (https://gka.github.io/chroma.js/#color-scales)
+    const colourscale_full = colourscales[config.colourscale](vmin, vmax);
+    const colourscale_norm = colourscales[config.colourscale](0, 100);
+    obj.updateColours(r => colourscale_full(obj.mapping.hexes[r][key]));
+    obj.extra.colourbar && updateColourbar(obj.extra.colourbar, colourscale_norm, vmin, vmax);
+    obj.extra.activeKey = key;
+
+    // Reset gridcells
+    [...obj.el.querySelectorAll('.hex-cell.hover')].forEach(node => node.classList.remove('hover'));
+  }
+
+  function updateColourbar(el, colourscale, vmin, vmax) {
+    const inner = el.querySelector('.hexmap__colourbar__inner');
+    inner.innerHTML = '';
+    for (let i = 0; i < 100; i++) {
+      const span = document.createElement('span');
+      span.style.backgroundColor = colourscale(i);
+      inner.appendChild(span)
+    }
+
+    el.querySelector('.hexmap__colourbar__min').innerText = vmin;
+    el.querySelector('.hexmap__colourbar__max').innerText = vmax;
   }
 })();
